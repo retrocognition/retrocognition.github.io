fd6cc196222c205151dd0fdc66b315059bfe4d36
marksulls <marksulls@gmail.com>
2013-09-07 19:46:34 -0600

    initial dist subtree commit

---
+++ dist/scripts/f002ac72.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("retrocognition",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("retrocognition.github.ioApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
