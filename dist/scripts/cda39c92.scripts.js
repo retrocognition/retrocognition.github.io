aece7dffe4f4e5f0696392e447d5b0fad844d48f
marksulls <marksulls@gmail.com>
2013-10-06 13:32:29 -0600

    v1

--- dist/scripts/cda39c92.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("retrocognition.github.ioApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("retrocognition.github.ioApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
